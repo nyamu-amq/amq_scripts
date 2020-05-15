35bd7e8123e66b93a9e1648353c17ed0d452ea2b
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-15 11:44:12 +0900

    Add files via upload

---
+++ WheelVolumeControl.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Wheel Volume Control
+// @namespace    https://github.com/nyamu-amq
+// @version      0.1
+// @author       nyamu
+// @match        https://animemusicquiz.com/*
+// @grant        none
+
+// ==/UserScript==
+
+if (!window.setupDocumentDone) return;
+
+$("#qpAnimeCenterContainer")
+	.on("wheel", volumeControl);
+$("#qpAvatarRow")
+	.on("wheel", volumeControl);
+
+function volumeControl(event) {
+	var volumetemp=volumeController.volume;
+	volumetemp+=(event.originalEvent.deltaY<0)?.05:-.05;
+	volumetemp=Math.min(Math.max(volumetemp, 0), 1);
+	volumeController.volume=volumetemp;
+	volumeController.adjustVolume();
+}
