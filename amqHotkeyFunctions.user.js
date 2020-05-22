9867b346bc84ab5a83195bb3333bb32068814957
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-23 08:30:54 +0900

    Add files via upload

--- amqHotkeyFunctions.user.js
+++ amqHotkeyFunctions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Hotkey Functions
 // @namespace    https://github.com/nyamu-amq
-// @version      0.1
+// @version      0.2
 // @description  enable hotkey functions
 // @description  ESC: remove zombie tooltips
 // @description  TAB: move cursor focus to chat box and answer box
@@ -12,15 +12,19 @@
 // ==/UserScript==
 
 function doc_keyUp(event) {
-	console.log(event.keyCode);
 	if (event.keyCode == '27') {
 		$("[id^=tooltip]").remove(); $("[id^=popover]").remove();
 	}
 	else if (event.keyCode == '9') {
-		if(document.activeElement.id=="qpAnswerInput") {
+		var inputContainer = $("#qpAnswerInputContainer");
+		if(document.activeElement.id=="qpAnswerInput" || $("#qpAnswerInput").attr("disabled")=="disabled") {
+			if(inputContainer.hasClass("focused")) {
+				inputContainer.removeClass("focused");
+			}
 			$("#gcInput").focus();
 		}
 		else {
+			inputContainer.addClass("focused");
 			$("#qpAnswerInput").focus();
 		}
 	}
