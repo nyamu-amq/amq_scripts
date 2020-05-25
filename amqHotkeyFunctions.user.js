8c0f565e75a752b27a2805664f0ed3ba2e158c4b
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-26 08:07:07 +0900

    added some functions

--- amqHotkeyFunctions.user.js
+++ amqHotkeyFunctions.user.js
@@ -1,10 +1,14 @@
 // ==UserScript==
 // @name         AMQ Hotkey Functions
 // @namespace    https://github.com/nyamu-amq
-// @version      0.3
+// @version      0.4
 // @description  enable hotkey functions
 // @description  ESC: remove zombie tooltips
 // @description  TAB: move cursor focus to chat box and answer box
+// @description  PgUp: increase volume
+// @description  PgDn: decrease volume
+// @description  Ctrl + M : toggle mute
+// @description  Shift + Enter: skip
 // @author       nyamu
 // @match        https://animemusicquiz.com/*
 // @grant        none
@@ -12,10 +16,10 @@
 // ==/UserScript==
 
 function doc_keyUp(event) {
-	if (event.keyCode == '27') {
+	if(event.keyCode=='27') {
 		$("[id^=tooltip]").remove(); $("[id^=popover]").remove();
 	}
-	else if (event.keyCode == '9') {
+	else if(event.keyCode=='9') {
 		if(quiz.answerInput.inFocus || quiz.isSpectator) {
 			quiz.setInputInFocus(false);
 			$("#gcInput").focus();
@@ -26,5 +30,30 @@ function doc_keyUp(event) {
 			$("#qpAnswerInput").focus();
 		}
 	}
+	else if(event.keyCode=='13' && !quiz.isSpectator && event.shiftKey) {
+		quiz.skipClicked()
+    }
+	else if(event.keyCode=='77' && event.ctrlKey) {
+		volumeController.setMuted(!volumeController.muted);
+		volumeController.adjustVolume();
+    }
+}
+
+function doc_keyDown(event) {
+	if(event.keyCode=='33') {
+		AdjustVolume(.05);
+    }
+	else if(event.keyCode=='34') {
+		AdjustVolume(-.05);
+    }
+}
+function AdjustVolume(amount) {
+	var volumetemp=Cookies.get('volume')*1;
+	volumetemp=volumetemp+amount;
+	volumetemp=Math.min(Math.max(volumetemp, 0), 1);
+	volumeController.volume=volumetemp;
+	volumeController.adjustVolume();
+	volumeController.setMuted(false);
 }
 document.addEventListener('keyup', doc_keyUp, false);
+document.addEventListener('keydown', doc_keyDown, false);
