9aa57abecc85e4940ec8da15d6f1264367ab9984
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-25 17:48:29 +0900

    Add files via upload

---
+++ amqNoFriendRoomFilter.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         AMQ No Friend Room Filter
+// @namespace    https://github.com/nyamu-amq
+// @version      0.1
+// @description  add no friend room filter
+// @author       nyamu
+// @match        https://animemusicquiz.com/*
+// @grant        none
+
+// ==/UserScript==
+
+(function() {
+    if (document.getElementById('startPage')) {
+        return
+    }
+
+	$("#rbMajorFilters").css("width", 350);
+	$("#rbMajorFilters").css("left", -350);
+	$("#rbMajorFilters").append(`<div><div class="customCheckbox"><input type="checkbox" id="rbfNoFriend"><label for="rbfNoFriend"><i class="fa fa-check" aria-hidden="true"></i></label></div><p>NoFriend</p><div>`);
+
+	$("#rbfNoFriend").on('click', () => {
+		roomBrowser.applyTileFilter();
+	});
+
+	roomBrowser.applyTileFilterToRoom = function (room) {
+		var nofriend = $("#rbfNoFriend").prop('checked') && Object.keys(room._friendsInGameMap).length==0;
+	    room.setHidden(nofriend || !roomFilter.testRoom(room));
+		this.updateNumberOfRoomsText();
+	};
+})();
