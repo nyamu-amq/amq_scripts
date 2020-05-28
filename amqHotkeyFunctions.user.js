d6682f1900b639624e07f1a7257ff494b82af3f4
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2020-05-28 11:05:17 +0900

    added box navigation

--- amqHotkeyFunctions.user.js
+++ amqHotkeyFunctions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Hotkey Functions
 // @namespace    https://github.com/nyamu-amq
-// @version      0.4
+// @version      0.5
 // @description  enable hotkey functions
 // @description  ESC: remove zombie tooltips
 // @description  TAB: move cursor focus to chat box and answer box
@@ -9,6 +9,9 @@
 // @description  PgDn: decrease volume
 // @description  Ctrl + M : toggle mute
 // @description  Shift + Enter: skip
+// @description  Shift + PgUp: move box focus to upper box
+// @description  Shift + PgDn: move box focus to lower box
+// @description  Shift + Home: move box focus to my box
 // @author       nyamu
 // @match        https://animemusicquiz.com/*
 // @grant        none
@@ -32,20 +35,42 @@ function doc_keyUp(event) {
 	}
 	else if(event.keyCode=='13' && !quiz.isSpectator && event.shiftKey) {
 		quiz.skipClicked()
-    }
+	}
 	else if(event.keyCode=='77' && event.ctrlKey) {
 		volumeController.setMuted(!volumeController.muted);
 		volumeController.adjustVolume();
-    }
+	}
 }
 
 function doc_keyDown(event) {
-	if(event.keyCode=='33') {
-		AdjustVolume(.05);
-    }
-	else if(event.keyCode=='34') {
-		AdjustVolume(-.05);
-    }
+	if(event.shiftKey) {
+		var maxgroup=Object.keys(quiz.groupSlotMap).length;
+		if(maxgroup>1) {
+			var curgroup=parseInt(quiz.avatarContainer.currentGroup);
+			if(event.keyCode=='33') {
+				if(curgroup>1)
+					quiz.selectAvatarGroup(String(curgroup-1));
+			}
+			else if(event.keyCode=='34') {
+				if(curgroup<maxgroup)
+					quiz.selectAvatarGroup(String(curgroup+1));
+			}
+			else if(event.keyCode=='36') {
+				if(!quiz.isSpectator) {
+					if(quiz.ownGroupSlot!=quiz.avatarContainer.currentGroup)
+						quiz.selectAvatarGroup(quiz.ownGroupSlot);
+				}
+			}
+		}
+	}
+	else {
+		if(event.keyCode=='33') {
+			AdjustVolume(.05);
+		}
+		else if(event.keyCode=='34') {
+			AdjustVolume(-.05);
+		}
+	}
 }
 function AdjustVolume(amount) {
 	var volumetemp=Cookies.get('volume')*1;
