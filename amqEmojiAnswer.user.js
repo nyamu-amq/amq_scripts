fbc27ae41ae710d04f68d81b84e6c4ca546fd4b9
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2021-07-24 03:40:34 +0900

    fixed for update

--- amqEmojiAnswer.user.js
+++ amqEmojiAnswer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Emoji Answer
 // @namespace    https://github.com/nyamu-amq
-// @version      0.3
+// @version      0.4
 // @description  convert emoji shortcode in answer box when press enter
 // @author       nyamu
 // @match        https://animemusicquiz.com/*
@@ -18,15 +18,14 @@ $("#qpAnswerInput").unbind("click keypress");
 $("#qpAnswerInput").keypress((event) => {
 	if (event.which === 13) {
 		let answer=$("#qpAnswerInput").val();
-		$("#qpAnswerInput").val(translateShortcodeToUnicode(answer));
-		quiz.answerInput.submitAnswer(true);
+        quiz.answerInput.setNewAnswer(translateShortcodeToUnicode(answer).text);
 	}
 });
 
 $("#mhRoomNameInput").keypress((event) => {
 	if (event.which === 13) {
 		let text=$("#mhRoomNameInput").val();
-		$("#mhRoomNameInput").val(translateShortcodeToUnicode(text));
+		$("#mhRoomNameInput").val(translateShortcodeToUnicode(text).text);
 	}
 });
 
