8f407ecd87595917a4a94015f6d520b08160ef47
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2021-07-27 07:07:34 +0900

    Add files via upload

---
+++ amqAvoidTroubleShooterPopup.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         AMQ Aviod Trouble Shooter Popup
+// @namespace    https://github.com/nyamu-amq
+// @version      0.1
+// @description  for video buffering experts
+// @author       nyamu
+// @match        https://animemusicquiz.com/*
+// @grant        none
+
+// ==/UserScript==
+
+if (document.getElementById('startPage')) {
+    return
+}
+
+troubleShooterDropDown.videoIssueBuffering = function() {}
