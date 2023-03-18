a4d7160f61f66156b4522ba674086ae53d9d9b88
nyamu-amq <60846124+nyamu-amq@users.noreply.github.com>
2023-03-18 20:36:11 +0900

    Add files via upload

--- amqHotkeyFunctions.user.js
+++ amqHotkeyFunctions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Hotkey Functions
 // @namespace    https://github.com/nyamu-amq
-// @version      0.15
+// @version      0.16
 // @description  enable hotkey functions
 // @description  ESC: remove zombie tooltips
 // @description  TAB: move cursor focus to chat box and answer box
@@ -68,6 +68,50 @@ function doc_keyUp(event) {
 			quiz.setInputInFocus(false);
 		}
 	}
+	else if(lobby.inLobby && event.ctrlKey && hostModal.gameMode !== 'Ranked') {
+		if(event.keyCode=='37') {
+			if(lobby.isSpectator) {
+				let changeToListner = new Listener("Change To Player", function (succes) {
+					if (!succes) {
+						displayMessage("Error changing to player");
+					}
+					changeToListner.unbindListener();
+				}.bind(lobby));
+				changeToListner.bindListener();
+
+				socket.sendCommand({
+					type: "lobby",
+					command: "change to player"
+				});
+			}
+			else if(!lobby.isHost) {
+				lobby.isReady = !lobby.isReady;
+				socket.sendCommand({
+					type: "lobby",
+					command: "set ready",
+					data: { ready: lobby.isReady }
+				});
+				lobby.updateMainButton();
+			}
+		}
+		else if(event.keyCode=='39') {
+			if(!lobby.isSpectator) {
+				lobby.changeToSpectator(selfName);
+			}
+		}
+		else if(event.keyCode=='38') {
+			if(lobby.isHost && isAllPlayerReady()) {
+				lobby.fireMainButtonEvent();
+			}
+		}
+	}
+	else if(quiz.inQuiz && hostModal.gameMode !== 'Ranked') {
+		if(event.keyCode=='40' && event.ctrlKey) {
+			if(lobby.isHost) {
+				quiz.startReturnLobbyVote();
+			}
+		}
+	}
 }
 
 let answerinput;
@@ -82,8 +126,6 @@ function getFocused() {
 		if(answerinput[i]==focused) return i;
 	}
 	return -1;
-}
-function focusInput() {
 }
 
 function isAllPlayerReady() {
